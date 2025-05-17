78e55ad6f45ad151e1c960cfb53515b234c1622c
Eric Stevens <ericstevens26101@gmail.com>
2025-05-16 17:07:25 -0700

    5 hour tokens

--- lib/audio-tour-infrastructure-stack.ts
+++ lib/audio-tour-infrastructure-stack.ts
@@ -108,8 +108,8 @@ export class AudioTourInfrastructureStack extends cdk.Stack {
         userSrp: true,
       },
       generateSecret: false,
-      accessTokenValidity: cdk.Duration.minutes(5),
-      idTokenValidity: cdk.Duration.minutes(5),
+      accessTokenValidity: cdk.Duration.minutes(300),
+      idTokenValidity: cdk.Duration.minutes(300),
       refreshTokenValidity: cdk.Duration.days(365), // 1 year token validity
     });
 
